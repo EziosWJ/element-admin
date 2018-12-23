600d79cc540fb31a7592ba9fbeb03d6d3a7cd7d7
Wangjian <656876071@qq.com>
2018-12-23 13:13:11 +0800

    菜单头和侧菜单

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,69 +1,37 @@
 <template>
   <div class="hello">
-<el-container>
-  <el-header>Header</el-header>
-  <el-container>
-    <el-aside>
-      <el-row class="tac">
-  <el-col :span="12">
-    <h5>默认颜色</h5>
-    <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
-        </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
-    </el-menu>
-  </el-col>
-</el-row>
-    </el-aside>
-    <el-container>
-      <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
+    <el-container direction="vertical">
+      <el-header>
+        <header-nav/>
+      </el-header>
+      <el-container>
+        <el-aside width="300px">
+          <slider-nav/>
+        </el-aside>
+        <el-main>main</el-main>
+      </el-container>
+      <el-footer>
+        footer @copyright 2018
+      </el-footer>
     </el-container>
-  </el-container>
-</el-container>
   </div>
 </template>
 
 <script>
+import HeaderNav from "./commons/HeaderNav";
+import SliderNav from "./commons/SliderNav";
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  components: {
+    HeaderNav,
+    SliderNav
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
